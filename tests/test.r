c4b982b01a70e5a1366501295266b04d899ca794
Cristian Roman <cromanpa94@gmail.com>
2022-03-17 09:08:56 -0700

    basicTemplate

--- tests/test.r
+++
@@ -1,7 +0,0 @@
-library(digest)
-#digest("B", algo='sha1') == digest("A", algo='sha1')
-
-if("A"" = "A"){
-              quit(status = 1)}else{
-               quit(status = 0)
-              }
--- tests/test.sh
+++
@@ -1,9 +0,0 @@
-echo "Running tests..."
-if Rscript --vanilla tests/test.R ; then
- echo "Pass: Program exited zero"
-else
- echo "Fail: Program did not exit zero"
- exit 1
-fi
-echo "All tests passed."
-exit 0
